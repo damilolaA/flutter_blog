26ff7598dadd0b3f0018ccf33989a6b75ba7eee0
damilolaa <oadio@tradedepot.co>
2019-05-17 19:40:26 +0100

    implement TabBar view on home widget

--- lib/widgets/scenes/home/home.dart
+++ lib/widgets/scenes/home/home.dart
@@ -5,15 +5,34 @@ import '../../../bloc/index.dart';
 import '../../../styles/index.dart';
 import '../../../blog_app.dart';
 
-class Main extends StatelessWidget {
-  Main(this.options, this.onChanged);
-
+class Main extends StatefulWidget {
   final BlogOptions options;
   final onChanged;
+
+  Main(this.options, this.onChanged);
+
+  MainState createState() => MainState();
+}
+class MainState extends State<Main> with SingleTickerProviderStateMixin {
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 3);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   Widget _buildAppBar(context) {
     return AppBar(
       backgroundColor: white,
+      centerTitle: true,
       title: Center(
         child: Text(
           "HYPEBEAST",
@@ -30,6 +49,27 @@ class Main extends StatelessWidget {
           onPressed: () {},
         )
       ],
+      bottom: TabBar(
+        controller: _tabController, 
+        indicatorColor: black,
+        tabs: <Widget>[
+          Tab(
+            child: Container(
+              child: Text("YOUR FEED", style: Theme.of(context).textTheme.body2.copyWith(color: black)),
+            ),
+          ),
+          Tab(
+            child: Container(
+              child: Text("LATEST", style: Theme.of(context).textTheme.body2.copyWith(color: black)),
+            ),
+          ),
+          Tab(
+            child: Container(
+              child: Text("POPULAR", style: Theme.of(context).textTheme.body2.copyWith(color: black)),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
@@ -39,35 +79,35 @@ class Main extends StatelessWidget {
 
     return Scaffold(
       appBar: _buildAppBar(context),
-      body: Center(
-        child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                child: Switch(
-                    value: options.theme == kDarktheme,
-                    onChanged: (bool value) {
-                      onChanged(options.copyWith(
-                          theme: value ? kDarktheme : kLighttheme));
-                    },
-                    activeColor: const Color(0xFF39CEFD),
-                    activeTrackColor: isDark ? Colors.white30 : Colors.black26),
-              ),
-              Container(
-                  child: BlocBuilder<CounterEvent, int>(
-                bloc: _userBloc,
-                builder: (BuildContext context, int count) {
-                  return Text(
-                    '$count',
-                    style: TextStyle(fontSize: 24.0),
-                  );
-                },
-              )),
-            ],
-          ),
-        ),
-      ),
+      // body: Center(
+      //   child: SafeArea(
+      //     child: Column(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: <Widget>[
+      //         Container(
+      //           child: Switch(
+      //               value: widget.options.theme == kDarktheme,
+      //               onChanged: (bool value) {
+      //                 widget.onChanged(widget.options.copyWith(
+      //                     theme: value ? kDarktheme : kLighttheme));
+      //               },
+      //               activeColor: const Color(0xFF39CEFD),
+      //               activeTrackColor: isDark ? Colors.white30 : Colors.black26),
+      //         ),
+      //         Container(
+      //             child: BlocBuilder<CounterEvent, int>(
+      //           bloc: _userBloc,
+      //           builder: (BuildContext context, int count) {
+      //             return Text(
+      //               '$count',
+      //               style: TextStyle(fontSize: 24.0),
+      //             );
+      //           },
+      //         )),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
