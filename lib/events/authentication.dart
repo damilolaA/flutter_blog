3e1192ee7481a10b413bb179ac6e1a89ae163761
damilolaa <oadio@tradedepot.co>
2019-05-18 01:34:56 +0100

    install equatable and setup authentication event

---
+++ lib/events/authentication.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+abstract class AuthenticationEvent extends Equatable {
+  AuthenticationEvent([List props = const [] ]) : super(props);
+}
+
+class AppStarted extends AuthenticationEvent {
+  @override 
+  String toString() => "AppStarted";
+}
